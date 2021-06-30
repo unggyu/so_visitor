c5a1da8eb169f178cb0e374b965ec883d4a2d6aa
Choi-UngGyu <cdr982116@gmail.com>
2021-07-01 02:03:22 +0900

    Edit index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Mailgun = require('mailgun-js');
 require('dotenv').config();
 
 let mailgun;
+const LOGIN_PAGE = 'https://stackoverflow.com/users/login';
 const hasApiKey = !isEmpty(process.env.MAILGUN_API_KEY);
 const hasDomain = !isEmpty(process.env.MAILGUN_DOMAIN);
 console.log(`Has mailgun api key? ${hasApiKey}`);
@@ -15,55 +16,6 @@ if (hasApiKey && hasDomain) {
     domain: process.env.MAILGUN_DOMAIN
   });
 }
-
-function isEmpty(value) {
-  return value == '' ||
-    value == null ||
-    value == undefined ||
-    (value != null &&
-      typeof value == "object" &&
-      !Object.keys(value).length);
-}
-
-function processResult(text) {
-  console.log(`Visit success. (${text})`);
-  if (mailgun) {
-    mailgun
-      .messages()
-      .send({
-        from: 'SO Visitor <no-reply@mailgun.org>',
-        to: process.env.EMAIL,
-        subject: `Stackoverflow visiting report (${text})`,
-        text: `Hi man. Thats your stat for now: ${text}`
-      }, (error, body) => {
-        if (error) {
-          throw error;
-        }
-    });
-    console.log('Mail has been sent.')
-  }
-}
-
-function processError(errText) {
-  console.log(`Visit failure. error: ${errText}`);
-  if (mailgun) {
-    mailgun
-      .messages()
-      .send({
-        from: 'SO Visitor <no-reply@mailgun.org>',
-        to: process.env.EMAIL,
-        subject: `Stackoverflow visiting report (Error)`,
-        text: `Something went wrong: ${errText}`
-      }, (error, body) => {
-        if (error) {
-          throw error;
-        }
-    });
-    console.log('Mail has been sent.');
-  }
-}
-
-const LOGIN_PAGE = 'https://stackoverflow.com/users/login';
 
 nightmare
   .goto(LOGIN_PAGE)
@@ -81,3 +33,55 @@ nightmare
   .end()
   .then(processResult)
   .catch(processError);
+
+function isEmpty(value) {
+  return value == '' ||
+    value == null ||
+    value == undefined ||
+    (value != null &&
+      typeof value == "object" &&
+      !Object.keys(value).length);
+}
+
+function processResult(text) {
+  console.log(`Visit success. (${text})`);
+  if (mailgun) {
+    mailgun
+      .messages()
+      .send({
+        from: 'SO Visitor <no-reply@mailgun.org>',
+        to: process.env.SO_EMAIL,
+        subject: `Stackoverflow visiting report (${text})`,
+        text: `Hi man. Thats your stat for now: ${text}`
+      }, (error, body) => {
+        if (error) {
+          console.log(`An error occurred while sending mail. error: ${error}`);
+          throw error;
+        } else {
+          console.log('Mail has been sent.');
+        }
+    });
+  }
+}
+
+function processError(errText) {
+  console.log(`Visit failure. error: ${errText}`);
+  if (mailgun) {
+    mailgun
+      .messages()
+      .send({
+        from: 'SO Visitor <no-reply@mailgun.org>',
+        to: process.env.SO_EMAIL,
+        subject: `Stackoverflow visiting report (Error)`,
+        text: `Something went wrong: ${errText}`
+      }, (error, body) => {
+        if (error) {
+          console.log(`An error occurred while sending mail. error: ${error}`);
+          throw error;
+        } else {
+          console.log('Mail has been sent.');
+        }
+    });
+    console.log('Mail has been sent.');
+  }
+}
