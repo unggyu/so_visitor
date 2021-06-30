281de01a3acc8cef4bd3bbe3e0ff8b70fff9befc
Choi-UngGyu <cdr982116@gmail.com>
2021-06-30 22:40:43 +0900

    Modify index.js

--- index.js
+++ index.js
@@ -1,72 +1,70 @@
 const Nightmare = require('nightmare');
 const Mailgun = require('mailgun-js');
+require('dotenv').config();
 
-const confFile = require('./conf.json');
-
-const nightmare = Nightmare({show: false});
+const nightmare = Nightmare({ show: false });
 
 let mailgun = null;
-if (confFile.mailgun_api_key && confFile.mailgun_domain) {
-    mailgun = Mailgun({
-        apiKey: confFile.mailgun_api_key,
-        domain: confFile.mailgun_domain
-    });
+if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN) {
+  mailgun = Mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  });
 }
-
 
 const LOGIN_PAGE = 'https://stackoverflow.com/users/login';
 
 function processResult(text) {
-    if (mailgun) {
-        mailgun
-            .messages()
-            .send({
-                from: 'SO Visitor <no-reply@mailgun.org>',
-                to: confFile.email,
-                subject: `Stackoverflow visiting report (${text})`,
-                text: `Hi man. Thats your stat for now: ${text}`
-            }, (error, body) => {
-                if (error) {
-                    throw error;
-                }
-            });
-    }
+  if (mailgun) {
+    mailgun
+      .messages()
+      .send({
+        from: 'SO Visitor <no-reply@mailgun.org>',
+        to: process.env.EMAIL,
+        subject: `Stackoverflow visiting report (${text})`,
+        text: `Hi man. Thats your stat for now: ${text}`
+      }, (error, body) => {
+        if (error) {
+          throw error;
+        }
+    });
+  }
 }
 
 function processError(errText) {
-    if (mailgun) {
-        mailgun
-            .messages()
-            .send({
-                from: 'SO Visitor <no-reply@mailgun.org>',
-                to: confFile.email,
-                subject: `Stackoverflow visiting report (Error)`,
-                text: `Something went wrong: ${errText}`
-            }, (error, body) => {
-                if (error) {
-                    throw error;
-                }
-            });
-    }
+  if (mailgun) {
+    mailgun
+      .messages()
+      .send({
+        from: 'SO Visitor <no-reply@mailgun.org>',
+        to: process.env.EMAIL,
+        subject: `Stackoverflow visiting report (Error)`,
+        text: `Something went wrong: ${errText}`
+      }, (error, body) => {
+        if (error) {
+          throw error;
+        }
+    });
+  }
 }
 
 nightmare
-    .goto(LOGIN_PAGE)
-    .wait('#login-form')
-    .type('#email', confFile.email)
-    .type('#password', confFile.password)
-    .click('#submit-button')
-    .wait('a.my-profile')
-    .click('a.my-profile')
-    .wait('#top-cards')
-    .evaluate(() => {
-        const el = document.querySelector('#top-cards .js-highlight-box-badges span.fs-caption.grid--cell');
-        return el ? el.innerText : 'null';
-    })
-    .end()
-    .then(progressText => {
-        processResult(progressText);
-    })
-    .catch(function (error) {
-        processError(error);
-    });
+  .goto(LOGIN_PAGE)
+  .wait('#login-form')
+  .type('#email', process.env.EMAIL)
+  .type('#password', process.env.PASSWORD)
+  .click('#submit-button')
+  .wait('a.my-profile')
+  .click('a.my-profile')
+  .wait('#top-cards')
+  .evaluate(() => {
+    const el = document.querySelector('#top-cards .js-highlight-box-badges span.fs-caption.grid--cell');
+    return el ? el.innerText : 'null';
+  })
+  .end()
+  .then(progressText => {
+    processResult(progressText);
+  })
+  .catch(function (error) {
+    processError(error);
+  });
